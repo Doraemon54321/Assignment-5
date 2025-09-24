d9a0f75612123f8b32c6239be25562a8d64ad09e
Lê Minh Huy <leminhhuy08032005@gmail.com>
2025-09-24 14:13:31 +0700

    Create callback.js

---
+++ callback.js
@@ -0,0 +1,12 @@
+function fetchData(callback) {
+    setTimeout(() => {
+        const data = { name: "John", age: 30 };
+        callback(data);
+    }, 1000);
+}
+
+function handleData(result) {
+    console.log("Received data:", result);
+}
+
+fetchData(handleData);
